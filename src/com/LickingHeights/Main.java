9b7a171e573d28fc9cb6a7e09d0e2c17f1a8ce16
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-04-02 14:19:17 -0400

    Wrote the poem

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,10 +8,27 @@ public class Main {
 	Scanner keyboard;
 	keyboard = new Scanner(System.in);
 
-	
-	int userAge;
+	String userMonth;
+	String userDay1;
+	String userYear;
 
-	while(userAge>18);
+        System.out.println("Monday's child is fare of face,");
+        System.out.println("Tusday's child is full of grace,");
+        System.out.println("Wednesay's child is full of woe,");
+        System.out.println("Thursday's child has far to go,");
+        System.out.println("Friday's child is loving and giving,");
+        System.out.println("Saturday's child works hard for a living,");
+        System.out.println("But the child born on the Sabbath Day,");
+        System.out.println("Is fair and wise and good in every way.");
+        System.out.println("");
+        System.out.println("Lets see what day you where born on");
+        System.out.println("What month were you born (As a number)");
+        userMonth = keyboard.nextLine();
+        System.out.println("What day in that month were you born");
+        userDay1 = keyboard.nextLine();
+        System.out.println("What year were you born");
+        userYear = keyboard.ne
+
 
 
 
