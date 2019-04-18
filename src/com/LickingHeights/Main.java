6d064f8dd614cf1cc644bdd44d0029ccf794c7b4
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-04-18 13:32:05 -0400

    Added switch for months

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,7 +10,8 @@ public class Main {
 
 	int userYear1;
 	int userMonth;
-	int userDay;
+	int userDay2;
+	int userDay1;
 	int userYear2;
 
         System.out.println("Monday's child is fare of face,");
@@ -28,13 +29,85 @@ public class Main {
 
 
         System.out.println("What day in "+printMonth(userMonth)+" were you born");
-        userDay = keyboard.nextInt();
+        userDay1 = keyboard.nextInt();
         System.out.println("What year were you born");
         userYear1 = keyboard.nextInt();
 
 
 
         }
+    public static int userMonth2(int userMonth) {
+
+        switch (userMonth) {
+            case 1:
+                return 13;
+            break;
+            case 2:
+                return 14;
+            break;
+            case 3:
+                return 3;
+            break;
+            case 4:
+                return 4;
+            break;
+            case 5:
+                return 5;
+            break;
+            case 6:
+                return 6;
+            break;
+            case 7:
+                return 7;
+            break;
+            case 8:
+                return 8;
+            break;
+            case 9:
+                return 9;
+            break;
+            case 10:
+                return 10;
+            break;
+            case 11:
+                return 11;
+            break;
+            case 12:
+                return 12;
+            break;
+        }
+
+
+    public static int weekDay(int userDay ,int userMonth2,int userYear1){
+
+            weekDay = userDay + 13 [userMonth2 + 1]
+
+
+
+
+
+    }
+
+    }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //method that will take the number for the month
         //return the written month
 
@@ -45,36 +118,38 @@ public class Main {
         switch (userMonth) {
             case 1:
                 return "January";
-            case 2 :
+            case 2:
                 return "February";
-            case 3 :
+            case 3:
                 return "March";
-            case 4 :
+            case 4:
                 return "April";
-            case 5 :
+            case 5:
                 return "May";
-            case 6 :
+            case 6:
                 return "June";
-            case 7 :
+            case 7:
                 return "July";
-            case 8 :
+            case 8:
                 return "August";
-            case 9 :
+            case 9:
                 return "September";
-            case 10 :
+            case 10:
                 return "Octeber";
-            case 11 :
+            case 11:
                 return "November";
-            case 12 :
+            case 12:
                 return "December";
-            default :
+            default:
                 return "";
 
         }
-
-
     }
 }
+
+
+
+
 
 
 
