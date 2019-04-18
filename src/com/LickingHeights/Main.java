4109ea16ffa42ed2c365ab6696be6ac413e24193
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-04-18 14:18:04 -0400

    Started finishing the project

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,18 +1,22 @@
 package com.LickingHeights;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	Scanner keyboard;
-	keyboard = new Scanner(System.in);
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
 
-	int userYear1;
-	int userMonth;
-	int userDay2;
-	int userDay1;
-	int userYear2;
+        int userYear1;
+        int userMonth;
+        int userDay2;
+        int userDay1;
+        int userYear2;
+        int k;
+        int J;
+
 
         System.out.println("Monday's child is fare of face,");
         System.out.println("Tusday's child is full of grace,");
@@ -28,91 +32,67 @@ public class Main {
         userMonth = keyboard.nextInt();
 
 
-        System.out.println("What day in "+printMonth(userMonth)+" were you born");
+        System.out.println("What day in " + printMonth(userMonth) + " were you born");
         userDay1 = keyboard.nextInt();
         System.out.println("What year were you born");
         userYear1 = keyboard.nextInt();
 
 
+        System.out.println("Their are "+monthDays(userMonth)+" days in "+printMonth(userMonth)+"");
 
-        }
+
+
+    }
+
     public static int userMonth2(int userMonth) {
 
         switch (userMonth) {
             case 1:
                 return 13;
-            break;
+
             case 2:
                 return 14;
-            break;
+
             case 3:
-                return 3;
-            break;
             case 4:
-                return 4;
-            break;
             case 5:
-                return 5;
-            break;
             case 6:
-                return 6;
-            break;
             case 7:
-                return 7;
-            break;
             case 8:
-                return 8;
-            break;
             case 9:
-                return 9;
-            break;
             case 10:
-                return 10;
-            break;
             case 11:
-                return 11;
-            break;
             case 12:
-                return 12;
-            break;
+                return userMonth;
+
+            default:
+                return 41;
         }
 
 
-    public static int weekDay(int userDay ,int userMonth2,int userYear1){
+    }
 
-            weekDay = userDay + 13 [userMonth2 + 1]
+    public static int weekDay(int userDay, int userMonth2, int userYear1) {
+            userMonth2 = userMonth2(userMonth2);
+        if(userMonth2 >12){
+           userYear1--;
+        }
 
+        int K = (userYear1 % 100);
+        int J = (userYear1 / 100);
 
+        int weekDay = userDay + (13 * (userMonth2 + 1)) / 5 + K + (K / 4) + (J / 4) - (-2 * J);
 
+        return weekDay;
 
 
     }
 
-    }
 
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //method that will take the number for the month
-        //return the written month
+    //method that will take the number for the month
+    //return the written month
 
     public static String printMonth(int userMonth) {
-
 
 
         switch (userMonth) {
@@ -145,7 +125,43 @@ public class Main {
 
         }
     }
+
+    public static int monthDays (int userMonth){
+
+
+        switch (userMonth){
+             case 1:
+           return 31;
+             case 2:
+           return 28;
+             case 3:
+           return 31;
+             case 4:
+           return 30;
+             case 5:
+           return 31;
+             case 6:
+           return 31;
+             case 7:
+           return 31;
+             case 8:
+           return 31;
+             case 9:
+           return 30;
+             case 10:
+           return 31;
+             case 11:
+           return 30;
+             case 12:
+           return 31;
+           }
+    }
 }
+
+
+
+
+
 
 
 
